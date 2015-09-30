198bddf27dc1b20c04a2674b5eabecc88f4671e4
Harvey <zhanghfglad@gmail.com>
2015-09-30 16:19:35 -0400

    Initial Commit

---
+++ PlayStoryboardReferences/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PlayStoryboardReferences
+//
+//  Created by Harvey Zhang on 9/30/15.
+//  Copyright © 2015 HappyGuy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
